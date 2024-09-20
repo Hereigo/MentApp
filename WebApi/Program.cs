89e33ad53ecb5163366db0b6136fa93ec82e91e8
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-09-20 20:43:21 +0300

    refactoring

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,45 +9,21 @@ builder.Services.AddDbContext<ToDoListDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("ToDoListDb"));
 });
 
+builder.Services.AddAuthorization();
+
+// By default, both cookies and proprietary tokens are activated.
+// Cookies and tokens are issued at login if the useCookies query string parameter in the login endpoint is true.
 builder.Services
-    .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddIdentityApiEndpoints<IdentityUser>()
     .AddEntityFrameworkStores<ToDoListDbContext>();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Password settings.
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-    // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.AllowedForNewUsers = true;
-    // User settings.
-    options.User.AllowedUserNameCharacters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = false;
-});
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    // Cookie settings
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
-    options.LoginPath = "/Identity/Account/Login";
-    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-    options.SlidingExpiration = true;
-});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
 
 if (app.Environment.IsDevelopment())
 {
@@ -61,5 +37,16 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapSwagger().RequireAuthorization();
+// app.MapSwagger().RequireAuthorization("Admin");
+
+app.MapGet("/hello", (HttpContext httpContext) =>
+{
+    return "Hello people!";
+})
+.WithName("GetHello")
+.WithOpenApi()
+.RequireAuthorization();
 
 app.Run();
