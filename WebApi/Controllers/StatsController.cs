913b48fef136e5bdde6bd731b815e5e2ce0436e7
Hereigo <hereigo@online.ua>
2024-11-14 09:17:44 +0200

    renaming fix

--- WebApi/Controllers/StatsController.cs
+++ WebApi/Controllers/StatsController.cs
@@ -1,74 +1,24 @@
-﻿using Contracts.Commands;
-using Contracts.DTO;
-using Contracts.Queries;
-using Data.EF.Models;
-using Domain;
-using MediatR;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Services;
 
 namespace WebApi.Controllers;
 
-[Authorize]
+[Authorize(Roles = "SuperAdmin")]
 [ApiController]
 [Route("[controller]")]
-public class TasksController : ControllerBase
+public class StatsController : ControllerBase
 {
-    private readonly IMediator _mediator;
     private readonly StatsService _statsService;
-    private readonly UserManager<User> _userManager;
 
-    public TasksController(IMediator mediator, UserManager<User> userManager, StatsService statsService)
+    public StatsController(StatsService statsService)
     {
-        _mediator = mediator;
         _statsService = statsService;
-        _userManager = userManager;
     }
 
     [HttpGet]
     public IActionResult Index()
     {
-        _statsService.IncrementStatsCounter();
-        return Ok("This is a protected resource.");
-    }
-
-    [HttpGet("id")]
-    public async Task<IActionResult> GetTaskAsync(int id, CancellationToken cancellationToken)
-    {
-        var taskDetails = await _mediator.Send(new GetTaskQuery() { TaskId = id }, cancellationToken);
-        return taskDetails == null ? NotFound() : Ok(taskDetails);
-    }
-
-    [HttpGet("all")]
-    public async Task<IActionResult> GetAllTasksAsync(CancellationToken cancellationToken)
-    {
-        // Just Testing usersRoles:
-        var currentUser = await _userManager.GetUserAsync(this.User);
-        var isSuperAdmin = await _userManager.IsInRoleAsync(currentUser, "SuperAdmin");
-
-        var allTasks = await _mediator.Send(new GetAllTasksQuery() { }, cancellationToken);
-        return allTasks == null ? NotFound() : Ok(allTasks);
-    }
-
-    [HttpPost("new")]
-    public async Task<IActionResult> CreateTaskSimpleAsync(TaskApiDto taskDto, CancellationToken cancellationToken)
-    {
-        var currentUser = await _userManager.GetUserAsync(this.User);
-        if (currentUser != null)
-        {
-            var task = new TaskDetails
-            {
-                CategoryId = 1,
-                CreatedDate = DateTime.UtcNow,
-                Description = taskDto.Description,
-                Title = taskDto.Title,
-                UserId = currentUser.Id,
-            };
-
-            await _mediator.Send(new CreateTaskRequest() { Task = task }, cancellationToken);
-        }
-        return Ok(taskDto);
+        return Ok(_statsService.GetStatsCounter());
     }
 }
--- WebApi/Controllers/TasksController.cs
+++ WebApi/Controllers/TasksController.cs
@@ -1,24 +1,74 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Contracts.Commands;
+using Contracts.DTO;
+using Contracts.Queries;
+using Data.EF.Models;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Services;
 
 namespace WebApi.Controllers;
 
-[Authorize(Roles = "SuperAdmin")]
+[Authorize]
 [ApiController]
 [Route("[controller]")]
-public class StatsController : ControllerBase
+public class TasksController : ControllerBase
 {
+    private readonly IMediator _mediator;
     private readonly StatsService _statsService;
+    private readonly UserManager<User> _userManager;
 
-    public StatsController(StatsService statsService)
+    public TasksController(IMediator mediator, UserManager<User> userManager, StatsService statsService)
     {
+        _mediator = mediator;
         _statsService = statsService;
+        _userManager = userManager;
     }
 
     [HttpGet]
     public IActionResult Index()
     {
-        return Ok(_statsService.GetStatsCounter());
+        _statsService.IncrementStatsCounter();
+        return Ok("This is a protected resource.");
+    }
+
+    [HttpGet("id")]
+    public async Task<IActionResult> GetTaskAsync(int id, CancellationToken cancellationToken)
+    {
+        var taskDetails = await _mediator.Send(new GetTaskQuery() { TaskId = id }, cancellationToken);
+        return taskDetails == null ? NotFound() : Ok(taskDetails);
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllTasksAsync(CancellationToken cancellationToken)
+    {
+        // Just Testing usersRoles:
+        var currentUser = await _userManager.GetUserAsync(this.User);
+        var isSuperAdmin = await _userManager.IsInRoleAsync(currentUser, "SuperAdmin");
+
+        var allTasks = await _mediator.Send(new GetAllTasksQuery() { }, cancellationToken);
+        return allTasks == null ? NotFound() : Ok(allTasks);
+    }
+
+    [HttpPost("new")]
+    public async Task<IActionResult> CreateTaskSimpleAsync(TaskApiDto taskDto, CancellationToken cancellationToken)
+    {
+        var currentUser = await _userManager.GetUserAsync(this.User);
+        if (currentUser != null)
+        {
+            var task = new TaskDetails
+            {
+                CategoryId = 1,
+                CreatedDate = DateTime.UtcNow,
+                Description = taskDto.Description,
+                Title = taskDto.Title,
+                UserId = currentUser.Id,
+            };
+
+            await _mediator.Send(new CreateTaskRequest() { Task = task }, cancellationToken);
+        }
+        return Ok(taskDto);
     }
 }
